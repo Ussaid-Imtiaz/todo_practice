78748d0afdfb42abeea9564a54a6b4b69c1e97e9
Ussaid Imtiaz <imtiazussaid@gmail.com>
2024-05-31 11:42:40 +0500

    without changes of rehan

--- todo_practice/main.py
+++ todo_practice/main.py
@@ -6,10 +6,43 @@ from typing import AsyncGenerator
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from sqlmodel import SQLModel, create_engine, Session
+from todo_practice import setting
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Form
+from typing import Annotated
 
-from todo_practice.db import create_tables, get_session
-from todo_practice.models import Todo
-from todo_practice.route.user import user_router
+# Step-1: Create Database on Neon
+# Step-2: Create .env file for environment variables
+# Step-3: Create setting.py file for encrypting DatabaseURL
+
+# Create a variable using url as parameter and add psycopg with postgresql in url.
+connection_string: str = str(   # Convert URL into string
+    setting.DATABASE_URL
+    ).replace(      
+    "postgresql", 
+    "postgresql+psycopg"
+    )         
+
+# Step-4: Create engine using sqlmodel to create connection between SQLModel and Postgresql
+engine = create_engine( 
+    connection_string, 
+    # connect_args={"sslmode": "require"}, # use ssl(secure socket layer) to encrypt communication with DB.
+    pool_recycle=300, # SQL engine use pool of connections to minimise time of each request, recycle upto 5 mins (300seconds)
+    pool_size=10, 
+    echo=True
+    )  
+
+# Step-5: Create function for table creation. 
+def create_tables() -> None:
+    SQLModel.metadata.create_all(engine)    # Contents of Schema (with tables) is registered with metadata attribute of sqlmodel
+
+# Step-6: Create function for session management 
+def get_session():
+    with Session(engine) as session:        # With will auto close session.
+        yield session                       # yield (generator function) will iterate over sessions.
 
 
 # Step-7: Create contex manager for app lifespan
@@ -32,11 +65,15 @@ app : FastAPI = FastAPI(
             "description": "Development Server"
         }
     ]
-    ) 
+) 
 
-# include user route in app to run from here
-app.include_router(router=user_router)
 
+# Step-8: Create model class using tables to use as schema in APIs 
+class Todo(SQLModel, table = True):
+    id : int | None = Field(primary_key=True, default = None)
+    content: str = Field(index = True, min_length=3, max_length=54)
+    is_completed : bool = Field(default=False)
+    user_id : int = Field(foreign_key="user.id")
 
 # Step-9: Create all endpoints of todo app
 @app.get("/")
@@ -98,34 +135,6 @@ async def delete_todo(id: int, session:Annotated[Session, Depends(get_session)])
         return {"message": "Task Deleted Successfully"}
     else:
         raise HTTPException(status_code=404, detail="No Task Found")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
